16ef50a911c4383fb28fcca0f05428717d3b923f
WintBit <wintbit@gmail.com>
2024-07-25 12:18:33 +0800

    fix: guid

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -2,7 +2,7 @@
 
 public static class PluginInfo
 {
-    public const string PLUGIN_GUID = "cn.scutbot.rmserverhook";
+    public const string PLUGIN_GUID = "cn.scutbot.rmserverassist";
     public const string PLUGIN_NAME = "RMServerAssist";
     public const string PLUGIN_VERSION = "1.0.0";
 }
